60635d8fc0fc4f4c7a4ca37c6688a7507588511f
axelmusch <44018738+axelmusch@users.noreply.github.com>
2022-02-01 22:36:21 +0100

    button to change between sprite/3d

--- pokedex-react/src/components/PokemonCard.js
+++ pokedex-react/src/components/PokemonCard.js
@@ -17,9 +17,10 @@ export default function PokemonCard(props) {
     function toggleShiny() {
         setShowShiny(prevShiny => !prevShiny)
     }
+
     function toggle3D() {
         console.log("3d")
-        setModelLoaded(true)
+        setModelShown(prevModelShown => !prevModelShown)
     }
     let typeString = ""
     types.forEach(type => {
@@ -29,54 +30,71 @@ export default function PokemonCard(props) {
 
     React.useEffect(() => {
         console.log('effect for load', modelLoaded)
-        /*    var scene = new THREE.Scene();
-           var camera = new THREE.PerspectiveCamera(20, 1, 0.1, 1000);
-           var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-           renderer.setSize(200, 200);
-           document.getElementById(name).appendChild(renderer.domElement);
-   
-           var controls = new OrbitControls(camera, renderer.domElement);
-           controls.autoRotate = true
-           camera.position.z = 8;
-           camera.position.y = 0.5;
-           const light = new THREE.AmbientLight(0xffffff); // soft white light
-           light.intensity = 1
-           scene.add(light);
-           const loader = new GLTFLoader()
-           const dracoLoader = new DRACOLoader();
-           dracoLoader.setDecoderPath('https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/libs/draco/'); // use a full url path
-   
-           loader.dracoLoader = dracoLoader
-           loader.load('./models/' + name + '.glb', function (gltf) {
-               gltf.scene.traverse(child => {
-                   if (child.isMesh) {
-   
-   
-                   }
-                   if (child.isGroup) console.log(child)
-               })
-   
-   
-               scene.add(gltf.scene);
-               renderer.render(scene, camera);
-           });
-   
-           var animate = function () {
-               requestAnimationFrame(animate);
-               controls.update()
-               renderer.render(scene, camera);
-           };
-           animate(); */
-    }, [modelLoaded])
+        if (!modelLoaded && modelShown) {
+            var scene = new THREE.Scene();
+            var camera = new THREE.PerspectiveCamera(20, 1, 0.1, 1000);
+            var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+            let canvasWidth = document.getElementById(name).offsetWidth
+            renderer.setSize(canvasWidth, canvasWidth);
+            document.getElementById(name).appendChild(renderer.domElement);
+
+            var controls = new OrbitControls(camera, renderer.domElement);
+            controls.autoRotate = true
+            camera.position.z = 8;
+            camera.position.y = 0.5;
+            const light = new THREE.AmbientLight(0xffffff); // soft white light
+            light.intensity = 1
+            scene.add(light);
+            const loader = new GLTFLoader()
+            const dracoLoader = new DRACOLoader();
+            dracoLoader.setDecoderPath('https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/libs/draco/'); // use a full url path
+
+            loader.dracoLoader = dracoLoader
+            loader.load('./models/' + name + '.glb', function (gltf) {
+                gltf.scene.traverse(child => {
+                    if (child.isMesh) {
+
+
+                    }
+                    if (child.isGroup) console.log(child)
+                })
+
+
+                scene.add(gltf.scene);
+                renderer.render(scene, camera);
+            });
+
+            var animate = function () {
+                requestAnimationFrame(animate);
+                controls.update()
+                renderer.render(scene, camera);
+            };
+            animate();
+            //setModelLoaded(true)
+            window.addEventListener('resize', onWindowResize);
+            function onWindowResize() {
+                console.log("resize")
+                camera.aspect = 1;
+                canvasWidth = document.getElementById(name).offsetWidth
+                renderer.setSize(canvasWidth, canvasWidth);
+                camera.updateProjectionMatrix();
+
+            }
+
+        }
+
+    }, [modelShown])
 
     return (
         <div className="pokemoncard">
-            <img className="pokemoncard--sprite" onClick={toggle3D} src={showShiny ? sprites.front_shiny : sprites.front_default} />
-            <div id={name} className="pokemonCanvas"></div>
+            <p onClick={toggle3D} className="pokemoncard--3dVisibility noselect">{modelShown ? "Hide" : "Show"} 3D</p>
+
+            {!modelShown ?
+                <img className="pokemoncard--sprite" onClick={toggleShiny} src={showShiny ? sprites.front_shiny : sprites.front_default} /> :
+                <div id={name} className="pokemoncard--pokemonCanvas"></div>}
+
             <h2>{id}. {name}</h2>
             <p>{typeString}</p>
-
-            <p className="pokemoncard--3dvisibility">{modelShown ? "Hide" : "Show"} 3D</p>
         </div>
     )
 
